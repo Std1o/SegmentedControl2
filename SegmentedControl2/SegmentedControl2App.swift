d7a069bb2a94f73fe56933e1713b9bad71d760b8
Std1o <37378410+Std1o@users.noreply.github.com>
2021-04-24 16:52:10 +0300

    Initial commit

---
+++ SegmentedControl2/SegmentedControl2App.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentedControl2App.swift
+//  SegmentedControl2
+//
+//  Created by Илья on 24.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct SegmentedControl2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
